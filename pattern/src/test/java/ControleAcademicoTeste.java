ad9e573a49360394328ab6d2fcb0442a9ba59515
Adson-Macedo <agnsoft@hotmail.com>
2020-08-17 00:44:16 -0300

    Finalizacao teste ControleAcademico

--- pattern/src/test/java/ControleAcademicoTeste.java
+++ pattern/src/test/java/ControleAcademicoTeste.java
@@ -1,6 +1,8 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -73,10 +75,9 @@ public class ControleAcademicoTeste {
             ca.criarTurma("Noturno", "MAP", "123456789");
         });
     }
+
     @Test
     public void verificarMatricularAluno() throws Exception{
-        ControleAcademico ca = new ControleAcademico();
-
         ca.criarAluno("Adson", "123456789");
         ca.criarDisciplina("MAP");
         
@@ -85,5 +86,125 @@ public class ControleAcademicoTeste {
         });
     }
 
+    @Test
+    public void verificarAdicionarAula() throws Exception {
+        ca.criarDisciplina("MAP");
+        ca.criarProfessor("Sabrina Souto", "123456789");
+        ca.criarTurma("Noturno","MAP", "123456789");
+        ca.adicionarAula("MAP", "Noturno", "Segunda", "00:00");
 
+        Aula aula = new Aula("00:00", "Segunda", ca.getDisciplina("MAP"));
+        List<Aula> aulas = ca.getDisciplina("MAP").getTurmas().get(0).getHorario().getAulas();
+        
+
+        assertEquals(aulas.get(0).getDiaDaSemana(), aula.getDiaDaSemana());
+        assertEquals(aulas.get(0).getHorarioAula(), aula.getHorarioAula());
+    }
+
+    @Test
+    public void verificarGetHorarioAulas() throws Exception {
+        ca.criarDisciplina("MAP");
+        ca.criarDisciplina("Estruturas de Dados");
+        ca.criarProfessor("Sabrina Souto", "123456789");
+        ca.criarTurma("Noturno","MAP", "123456789");
+        ca.criarTurma("Noturno","Estruturas de Dados", "123456789");
+        ca.adicionarAula("MAP", "Noturno", "Segunda", "20:00");
+        ca.adicionarAula("MAP", "Noturno", "Quinta", "20:00");
+        ca.adicionarAula("Estruturas de Dados", "Noturno", "Segunda", "18:00");
+        ca.adicionarAula("Estruturas de Dados", "Noturno", "Quarta", "18:00");
+
+        List<String> horarioAulas = ca.getHorarioAulas("123456789");
+        
+        assertEquals(horarioAulas.get(0), "Segunda\t18:00\tEstruturas de Dados");
+        assertEquals(horarioAulas.get(1), "Segunda\t20:00\tMAP");
+        assertEquals(horarioAulas.get(2), "Quarta\t18:00\tEstruturas de Dados");
+        assertEquals(horarioAulas.get(3), "Quinta\t20:00\tMAP");
+    }
+
+    @Test
+    public void verificarGetAlunosDaDisciplina() throws Exception {
+        ca.criarDisciplina("MAP");
+        ca.criarDisciplina("Estruturas de Dados");
+        ca.criarProfessor("Sabrina Souto", "123456789");
+        ca.criarAluno("Adson", "1111111111");
+        ca.criarAluno("Thairam", "2222222222");
+        
+        ca.criarTurma("Noturno","MAP", "123456789");
+        ca.criarTurma("Noturno","Estruturas de Dados", "123456789");
+        
+        ca.adicionarAula("MAP", "Noturno", "Segunda", "20:00");
+        ca.adicionarAula("Estruturas de Dados", "Noturno", "Segunda", "18:00");
+        
+        ca.matricularAluno("1111111111", "MAP", "Noturno");
+        ca.matricularAluno("1111111111", "Estruturas de Dados", "Noturno");
+        ca.matricularAluno("2222222222", "Estruturas de Dados", "Noturno");
+
+        List<String> alunos = ca.getAlunosDaDisciplina("Estruturas de Dados");
+        
+        assertEquals(alunos.get(0), "Matricula: 1111111111 Nome: Adson");
+        assertEquals(alunos.get(1), "Matricula: 2222222222 Nome: Thairam");
+    }
+
+    @Test
+    public void verificarGetDisciplinasDoAcademico() throws Exception {
+        ca.criarDisciplina("MAP");
+        ca.criarDisciplina("Estruturas de Dados");
+        ca.criarDisciplina("Discreta I");
+        
+        ca.criarProfessor("Sabrina Souto", "123456789");
+        ca.criarProfessor("Toin", "000000000");
+        
+        ca.criarAluno("Adson", "1111111111");
+        ca.criarAluno("Thairam", "2222222222");
+        
+        ca.criarTurma("Noturno","MAP", "123456789");
+        ca.criarTurma("Noturno","Estruturas de Dados", "123456789");
+        ca.criarTurma("Noturno","Discreta I", "000000000");
+        
+        ca.adicionarAula("MAP", "Noturno", "Segunda", "18:00");
+        ca.adicionarAula("MAP", "Noturno", "Quarta", "20:00");
+        ca.adicionarAula("Estruturas de Dados", "Noturno", "Quarta", "18:00");
+        ca.adicionarAula("Estruturas de Dados", "Noturno", "Quarta", "18:00");
+        ca.adicionarAula("Discreta I", "Noturno", "Quinta", "18:00");
+        ca.adicionarAula("Discreta I", "Noturno", "Terça", "20:00");
+
+        ca.matricularAluno("1111111111", "MAP", "Noturno");
+        ca.matricularAluno("1111111111", "Estruturas de Dados", "Noturno");
+        ca.matricularAluno("2222222222", "Discreta I", "Noturno");
+
+        List<String> disciplinas = ca.getDisciplinasDoAcademico("1111111111");
+        assertEquals(disciplinas.get(0), "Estruturas de Dados");
+        assertEquals(disciplinas.get(1), "MAP");
+    }
+
+    @Test
+    public void verificarGetNumeroDisciplinasAcademico() throws Exception {
+        ca.criarDisciplina("MAP");
+        ca.criarDisciplina("Estruturas de Dados");
+        ca.criarDisciplina("Discreta I");
+        
+        ca.criarProfessor("Sabrina Souto", "123456789");
+        ca.criarProfessor("Toin", "000000000");
+        
+        ca.criarAluno("Adson", "1111111111");
+        ca.criarAluno("Thairam", "2222222222");
+        
+        ca.criarTurma("Noturno","MAP", "123456789");
+        ca.criarTurma("Noturno","Estruturas de Dados", "123456789");
+        ca.criarTurma("Noturno","Discreta I", "000000000");
+        
+        ca.adicionarAula("MAP", "Noturno", "Segunda", "18:00");
+        ca.adicionarAula("MAP", "Noturno", "Quarta", "20:00");
+        ca.adicionarAula("Estruturas de Dados", "Noturno", "Quarta", "18:00");
+        ca.adicionarAula("Estruturas de Dados", "Noturno", "Quarta", "18:00");
+        ca.adicionarAula("Discreta I", "Noturno", "Quinta", "18:00");
+        ca.adicionarAula("Discreta I", "Noturno", "Terça", "20:00");
+
+        ca.matricularAluno("1111111111", "MAP", "Noturno");
+        ca.matricularAluno("1111111111", "Estruturas de Dados", "Noturno");
+        ca.matricularAluno("2222222222", "Discreta I", "Noturno");
+
+        assertEquals(ca.getDisciplinasDoAcademico("1111111111").size(), 2);
+        assertEquals(ca.getDisciplinasDoAcademico("000000000").size(), 1);
+    }
 }
