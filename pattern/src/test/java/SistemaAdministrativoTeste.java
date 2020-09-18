5fadf0d831e6de5e2c05254fb963aff9ee156c6f
Thairam <thayram01@gmail.com>
2020-09-17 22:09:30 -0300

    Padronizando os testes

--- pattern/src/test/java/SistemaAdministrativoTeste.java
+++ pattern/src/test/java/SistemaAdministrativoTeste.java
@@ -1,39 +1,34 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import sistemas.SistemaAdministrativo; 
+import sistemas.SistemaAdministrativo;
 
 public class SistemaAdministrativoTeste {
 
-    SistemaAdministrativo sistemaAdm = new SistemaAdministrativo();
+    SistemaAdministrativo sistema = new SistemaAdministrativo();
 
     @Test
     public void validarAdicionarReuniao() {
-        String matricula = "0012";
-        String horario = "08:00 - 10:00";
-        
-        String reuniao = sistemaAdm.adicionarReuniao(matricula, horario);
-        assertEquals(reuniao, "{matricula: '" + matricula + "', horario: '" + horario + "'"+ "}");
+        String resultadoEsperado = new String("{matricula: '001254', horario: '08:00 - 10:00'}");
+        assertEquals(sistema.adicionarReuniao("001254", "08:00 - 10:00"), resultadoEsperado);
     }
 
     @Test
     public void validarAdicionarEntrevista() {
-        String nomeEntrevistado = "Thairam Michel";
-        String horario = "15:30";
-        
-        String reuniao = sistemaAdm.adicionarEntrevista(nomeEntrevistado, horario);
-        assertEquals(reuniao, "{nomeEntrevistado: '" + nomeEntrevistado + "', horario: '" + horario + "'"+ "}");
-    }    
+        String resultadoEsperado = new String("{nomeEntrevistado: 'Thairam Michel', horario: '15:30'}");
+        assertEquals(sistema.adicionarEntrevista("Thairam Michel", "15:30"), resultadoEsperado);
+    }
 
     @Test
     public void validarObterAgendaDeReunioes() {
-        String resultadoEsperado = "[{matricula: '12345678', horario: '00:00'},\n{matricula: '87654321', horario: '10:00'}]";
-        assertEquals(sistemaAdm.obterAgendaDeReunioes() , resultadoEsperado);
-    }        
+        String resultadoEsperado = new String(
+                "[{matricula: '12345678', horario: '00:00'},\n{matricula: '87654321', horario: '10:00'}]");
+        assertEquals(sistema.obterAgendaDeReunioes(), resultadoEsperado);
+    }
 
     @Test
     public void validarObterAgendaDeEntrevistas() {
         String resultadoEsperado = "[{nomeEntrevistado: 'Fulano de Tal', horario: '00:00'},\n{nomeEntrevistado: 'Tal de Fulano', horario: '10:00'}]";
-        assertEquals(sistemaAdm.obterAgendaDeEntrevistas() , resultadoEsperado);
-    }            
+        assertEquals(sistema.obterAgendaDeEntrevistas(), resultadoEsperado);
+    }
 }
