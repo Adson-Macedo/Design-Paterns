292627b55533bef6e70441c97975fa4300f25ba0
Adson-Macedo <agnsoft@hotmail.com>
2020-11-09 21:27:25 -0300

    Finalizacao teste OnibusController

--- pattern/src/test/java/controller_test/OnibusControllerTeste.java
+++ pattern/src/test/java/controller_test/OnibusControllerTeste.java
@@ -1,29 +1,63 @@
 package controller_test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import controller.OnibusController;
 import model.Onibus;
+import model.Passageiro;
 
 public class OnibusControllerTeste {
     public OnibusController controller;
-
-    @BeforeAll
-    public void setup(){
-        this.controller = new OnibusController();
-    }
-
-    @Test
-    public void verificaAdicionarOnibus(){
+    
+    @BeforeEach
+    public void setup() {
         try{
-            Onibus onibus = new Onibus(10, "Campina Grande - Lagoa Seca");
-            assertTrue(controller.adicionarOnibus(onibus));
-        } catch (Exception e){
+            this.controller = new OnibusController();
+        }catch(Exception e){
             System.out.println(e.getMessage());
         }
     }
+    
+    @Test
+    public void verificaAdicionarOnibus()  throws Exception{
+        Onibus onibus = new Onibus(10, "Campina Grande - Lagoa Seca");
+        assertTrue(controller.adicionarOnibus(onibus));
+    }
+    
+    
+    @Test
+    public void verificarReservarPassagem() throws Exception{
+        Onibus onibus = new Onibus(10, "Campina Grande - Lagoa Seca");
+        Passageiro passageiro = new Passageiro("12345678910", "Joaozinho");
+        
+        assertTrue(controller.reservarPassagem(onibus, 10, passageiro));
+        assertThrows(Exception.class, () -> controller.reservarPassagem(onibus, 10, passageiro));
+    }
+    
+    @Test
+    public void verificarComprarPassagem()  throws Exception{
+        Onibus onibus = new Onibus(10, "Campina Grande - Lagoa Seca");
+        Passageiro passageiro = new Passageiro("12345678910", "Joaozinho");
+        
+        assertTrue(controller.comprarPassagem(onibus, 10, passageiro));
+        assertThrows(Exception.class, () -> controller.comprarPassagem(onibus, 10, passageiro));
+    }
+    
+    @Test
+    public void verificarCancelarReserva()  throws Exception{
+        Onibus onibus = new Onibus(10, "Campina Grande - Lagoa Seca");
+        Passageiro passageiro = new Passageiro("12345678910", "Joaozinho");
+        
+        controller.reservarPassagem(onibus, 8, passageiro);
+        controller.comprarPassagem(onibus, 7, passageiro);
+        assertTrue(controller.cancelarReserva(onibus, 8));
+        assertThrows(Exception.class,  () -> controller.cancelarReserva(onibus, 8));
+        assertThrows(Exception.class,  () -> controller.cancelarReserva(onibus, 7));
+    }
+
 
 }
